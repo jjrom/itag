844c1399213749ff0eb9ed7d4672a540f4dce6ee
jjrom <jerome.gasperi@gmail.com>
2021-08-26 15:52:33 +0200

    Correct itag function

--- build/itag-database/installDB.sh
+++ build/itag-database/installDB.sh
@@ -8,8 +8,8 @@ err_report() {
 }
 trap 'err_report $LINENO' ERR
 
-PGPASSWORD=${POSTGRES_PASSWORD} psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/00_itag_extensions.sql > /dev/null 2>&1
-PGPASSWORD=${POSTGRES_PASSWORD} psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/01_itag_functions.sql > /dev/null 2>&1
-PGPASSWORD=${POSTGRES_PASSWORD} psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/01_tamn.sql > /dev/null 2>&1
-PGPASSWORD=${POSTGRES_PASSWORD} psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/02_itag_model.sql > /dev/null 2>&1
+PGPASSWORD=${POSTGRES_PASSWORD} psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/00_itag_extensions.sql
+PGPASSWORD=${POSTGRES_PASSWORD} psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/01_itag_functions.sql
+PGPASSWORD=${POSTGRES_PASSWORD} psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/01_tamn.sql
+PGPASSWORD=${POSTGRES_PASSWORD} psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/02_itag_model.sql
 
--- build/itag-database/sql/01_itag_functions.sql
+++ build/itag-database/sql/01_itag_functions.sql
@@ -7,18 +7,18 @@
 --
 -- Create IMMUTABLE unaccent function 
 --
-CREATE OR REPLACE FUNCTION public.f_unaccent(text) RETURNS text AS
-$func$
-SELECT public.unaccent('public.unaccent', $1)  -- schema-qualify function and dictionary
-$func$  LANGUAGE sql IMMUTABLE;
+CREATE OR REPLACE FUNCTION public.f_unaccent(text)
+RETURNS TEXT AS $$
+    SELECT public.unaccent('public.unaccent', $1)  -- schema-qualify function and dictionary
+$$  LANGUAGE sql IMMUTABLE;
 
 --
 -- Return a transliterated version of input string in lowercase
 --
-CREATE OR REPLACE FUNCTION normalize(input TEXT, separator text DEFAULT '') 
-RETURNS TEXT AS $$
+CREATE OR REPLACE FUNCTION public.normalize(input text, separator text DEFAULT '') 
+RETURNS text AS $$
 BEGIN
-    RETURN translate(lower(public.f_unaccent(input)), ' '',:-\`\´\‘\’_' , separator);
+    RETURN translate(lower(public.f_unaccent(input)), ' '',:-`´‘’_' , separator);
 END
 $$ LANGUAGE 'plpgsql' IMMUTABLE;
 
@@ -26,7 +26,7 @@ $$ LANGUAGE 'plpgsql' IMMUTABLE;
 -- Return a transliterated version of input string which the first letter of each word
 -- is in uppercase and the remaining characters in lowercase
 --
-CREATE OR REPLACE FUNCTION normalize_initcap(input TEXT, separator text DEFAULT '') 
+CREATE OR REPLACE FUNCTION public.normalize_initcap(input TEXT, separator text DEFAULT '') 
 RETURNS TEXT AS $$
 BEGIN
     RETURN translate(initcap(public.f_unaccent(input)), ' '',:-\`\´\‘\’_' , separator);
